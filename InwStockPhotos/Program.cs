87211c7bcb64801ade667e8e62835d760c7149b1
Alex Loren <lorenalexm@gmail.com>
2023-02-17 06:26:18 -0800

    Converted to MudBlazor for views

--- InwStockPhotos/Program.cs
+++ InwStockPhotos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using InwStockPhotos;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
 
