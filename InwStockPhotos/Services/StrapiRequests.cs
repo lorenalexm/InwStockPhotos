2435e4561ca9a1d7e5301f0885d51d1e4105d30a
Alex Loren <lorenalexm@gmail.com>
2023-02-22 16:21:51 -0800

    Implemented method for getting a single model

--- InwStockPhotos/Services/StrapiRequests.cs
+++ InwStockPhotos/Services/StrapiRequests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using InwStockPhotos.Models;
 
 namespace InwStockPhotos.Services
 {
@@ -100,9 +101,32 @@ namespace InwStockPhotos.Services
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Performs a <see cref="RequestMethod.Get"/> request and creates a model from remote Strapi content.
+		/// </summary>
+		/// <typeparam name="T">A model that matches a Strapi content type.</typeparam>
+		/// <param name="id">The Strapi Id of the content to fetch.</param>
+		/// <returns>A complete model object of the <typeparamref name="T"/> type.</returns>
 		public async Task<T?> GetAsync<T>(int id)
 		{
-			throw new NotImplementedException();
+			string endpoint = "";
+			var type = typeof(T).ToString();
+			switch (type)
+			{
+				case "InwStockPhotos.Models.ContentBlock":
+					endpoint = ContentBlock.Endpoint;
+					break;
+
+				case "InwStockPhotos.Models.Tag":
+					endpoint = Tag.Endpoint;
+					break;
+
+				case "InwStockPhotos.Models.Image":
+					endpoint = Image.Endpoint;
+					break;
+			}
+
+			return await ExecuteAsync<T>($"{endpoint}/{id}", RequestMethod.Get);
 		}
 
 		public async Task<T?> UpdateAsync<T>(int id, object parameter)
